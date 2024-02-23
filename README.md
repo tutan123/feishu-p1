280ac56e0305413324c060d2a64a5f62c76ba905
tutan123 <823747469@qq.com>
2024-02-23 18:37:04 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## 飞书 sdk
+
+第三方Feishu库，目前只实现了使用的 `机器人`部分功能，后续会看精力继续完善
+
+### 安装
+
+```python
+pip install feishu-p1
+```
---
+++ setup.py
@@ -0,0 +1,36 @@
+# encoding: utf-8
+"""
+@author: liyao
+@contact: liyao2598330@126.com
+@software: pycharm
+@time: 2020/6/12 1:39 下午
+@desc:
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="feishu-p1",
+    version="1.0.1",
+    author="tutu",
+    author_email="tutu@gmail.com",
+    description="Feishu Third-party Libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liyao2598330/feishu-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+
+    ],
+    install_requires=[
+        'urllib3',
+        'requests'
+    ],
+    python_requires='>=2.7',
+)
